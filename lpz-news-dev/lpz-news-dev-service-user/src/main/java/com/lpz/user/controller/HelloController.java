b5f99cbb2e9eb2149f9e8622a604e656f7f57979
maomin <160663534@qq.com>
2020-11-20 17:34:58 +0800

    增加日志文件配置，以及多环境配置
    Co-Authored-By: MAO <maodan2006@gmail.com>

--- lpz-news-dev/lpz-news-dev-service-user/src/main/java/com/lpz/user/controller/HelloController.java
+++ lpz-news-dev/lpz-news-dev-service-user/src/main/java/com/lpz/user/controller/HelloController.java
@@ -1,7 +1,8 @@
 package com.lpz.user.controller;
 
 import com.lpz.api.controller.user.HelloControllerApi;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,7 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController implements HelloControllerApi {
 
+    final static Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     public Object hello() {
+        LOGGER.debug("debug: hello!");
+        LOGGER.info("info: hello!");
+        LOGGER.warn("warn: hello!");
+        LOGGER.error("error: hello!");
         return "hello";
     }
 }
